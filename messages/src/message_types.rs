9316fb803a306e91a0e3c17d47791a0e73756836
Andrew <asmith@owlshares.com>
2022-03-20 16:57:37 -0700

    upgraded udp2p to 0.1.5

--- messages/src/message_types.rs
+++ messages/src/message_types.rs
@@ -8,15 +8,6 @@ pub struct StateBlock(pub u128);
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum MessageType {
-    NetworkStateDataBaseMessage {
-        object: Vec<u8>,
-        data: Vec<u8>,
-        chunk_number: u32,
-        total_chunks: u32,
-        last_block: u128,
-        requestor: String,
-        sender_id: String,
-    },
     TxnMessage {
         txn: Vec<u8>,
         sender_id: String,
@@ -28,14 +19,6 @@ pub enum MessageType {
     BlockMessage {
         block: Vec<u8>,
         sender_id: String,
-    },
-    BlockChunkMessage {
-        sender_id: String,
-        requestor: String,
-        block_height: u128,
-        chunk_number: u128,
-        total_chunks: u128,
-        data: Vec<u8>,
     },
     ClaimMessage {
         claim: Vec<u8>,
@@ -90,64 +73,7 @@ pub enum MessageType {
     ClaimAbandonedMessage {
         claim: Vec<u8>,
         sender_id: String,
-    },
-    General {
-        data: Vec<u8>,
-    },
-    Identify {
-        data: String,
-        pubkey: String,
-    },
-    NewPeer {
-        data: Vec<u8>,
-        pubkey: String,
-    },
-    KnownPeers {
-        data: Vec<u8>,
-    },
-    AckMessage {
-        packet_id: String,
-        packet_number: u32,
-        src: String,
-    },
-    FirstHolePunch {
-        data: Vec<u8>,
-        pubkey: String,
-    },
-    SecondHolePunch {
-        data: Vec<u8>,
-        pubkey: String,
-    },
-    FinalHolePunch {
-        data: Vec<u8>,
-        pubkey: String,
-    },
-    InitHandshake {
-        data: Vec<u8>,
-        pubkey: String,
-        signature: String,
-    },
-    ReciprocateHandshake {
-        data: Vec<u8>,
-        pubkey: String,
-        signature: String,
-    },
-    CompleteHandshake {
-        data: Vec<u8>,
-        pubkey: String,
-        signature: String,
-    },
-    Ping {
-        data: Vec<u8>,
-        addr: Vec<u8>,
-        timestamp: Vec<u8>,
-    },
-    Pong {
-        data: Vec<u8>,
-        addr: Vec<u8>,
-        ping_timestamp: Vec<u8>,
-        pong_timestamp: Vec<u8>,
-    },
+    }
 }
 
 impl MessageType {
