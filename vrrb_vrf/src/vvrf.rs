3182d790bdec3787db7113e972d6486d779d0738
Anna <amouland@vrrb.io>
2022-08-15 09:15:24 -0400

    feat-vrf-crate-PR-one

--- vrrb_vrf/src/vvrf.rs
+++ vrrb_vrf/src/vvrf.rs
@@ -90,3 +90,102 @@ impl VRNG for VVRF {
         }).trim_start().to_string()
     }
 }
+
+
+///implement VVRF type by passing a secretKey such that
+///all the VVRF fields can now be calculated thanks to the sk being passed
+///and use of fxns defined below and imported
+impl VVRF {
+    pub fn new(message: &[u8], sk: SecretKey) -> VVRF {
+        let mut vrf = VVRF::generate_vrf(CipherSuite::SECP256K1_SHA256_TAI);
+        let pubkey = VVRF::generate_pubkey(&mut vrf, sk);
+        let (proof, hash) = VVRF::generate_seed(&mut vrf, message, sk).unwrap();
+        ///rng calculated from hash
+        let rng = ChaCha20Rng::from_seed(hash);
+        ///populate VVRF fields
+        VVRF {
+            vrf,
+            pubkey,
+            message: message.to_vec(),
+            proof,
+            hash,
+            rng: rng,
+        }
+    }
+
+    ///get a sk using SecretKey struct
+    fn generate_secret_key() -> SecretKey {
+        let secret_key = SecretKey::new(&mut rand::thread_rng());
+        secret_key
+    }
+
+    ///get vrf from openssl struct ECVRF (eliptic curve vrf)
+    fn generate_vrf(suite: CipherSuite) -> ECVRF {
+        ECVRF::from_suite(suite).unwrap()
+    }
+
+    ///get pk from vrf crate
+    fn generate_pubkey(vrf: &mut ECVRF, secret_key: SecretKey) -> Vec<u8> {
+        vrf.derive_public_key(&secret_key.secret_bytes()).unwrap()
+    }
+
+    ///generate seed
+    fn generate_seed(
+        vrf: &mut ECVRF,
+        message: &[u8],
+        secret_key: SecretKey,
+    ) -> Option<([u8; 81], [u8; 32])> {
+        if let Ok(pi) = vrf.prove(&secret_key.secret_bytes(), message) {
+            if let Ok(hash) = vrf.proof_to_hash(&pi) {
+                let mut proof_buff = [0u8; 81];
+                pi.iter().enumerate().for_each(|(i, v)| {
+                    proof_buff[i] = *v;
+                });
+                let mut hash_buff = [0u8; 32];
+                hash.iter().enumerate().for_each(|(i, v)| {
+                    hash_buff[i] = *v;
+                });
+
+                Some((proof_buff, hash_buff))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    ///check that hash and beta are equal to ensure hash(seed) is valid
+    pub fn verify_seed(&mut self) -> Result<(), InvalidProofError> {
+        if let Ok(beta) = self.vrf.verify(&self.pubkey, &self.proof, &self.message) {
+            if self.hash.to_vec() != beta {
+                return Err(InvalidProofError);
+            } else {
+                return Ok(());
+            }
+        } else {
+            return Err(InvalidProofError);
+        }
+    }
+
+    ///getter functions
+    pub fn get_pubkey(&self) -> Vec<u8> {
+        self.pubkey.clone()
+    }
+
+    pub fn get_message(&self) -> Vec<u8> {
+        self.message.clone()
+    }
+
+    pub fn get_proof(&self) -> [u8; 81] {
+        self.proof
+    }
+
+    pub fn get_hash(&self) -> [u8; 32] {
+        self.hash
+    }
+
+    pub fn get_rng(&self) -> ChaCha20Rng {
+        self.rng.clone()
+    }
+}
