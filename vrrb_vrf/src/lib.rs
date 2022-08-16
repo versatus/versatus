037cdeef0eb367e2419d973d8187957b84047113
Anna <90351322+parkedtruck@users.noreply.github.com>
2022-08-16 15:39:34 -0400

    feat vrf crate supports rng with range and random word/mnemonic generation
    * feat-add-vrf-crate
    
    * feat-vrf-rand-num
    
    * feat change to lib crate
    
    * branch change
    
    * feat-add-vrf-crate-mnemonics
    
    * new tests
    
    * feat-vrf-crate-PR-one
    
    * feat-add-range-to rng
    
    * feat-polish-range-tests
    
    * fix-add-gitignore-file
    
    * cached target
    
    * modify gitignores
    
    * pulled from vrrb_tui master
    
    Co-authored-by: Anna <amouland@vrrb.io>
    Co-authored-by: Anna Mouland <anna.mouland@aquanow.io>

---
+++ vrrb_vrf/src/lib.rs
@@ -0,0 +1,129 @@
+//! vrrb verifiable random function library crate
+//!
+//! generates a random number or random word/mnemonic phrase
+
+pub mod vrng;
+pub mod vvrf;
+//use crate::vrrb_vrf;
+
+///root module
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vrng::VRNG;
+    use crate::vvrf::VVRF;
+    use parity_wordlist::WORDS;
+    use secp256k1::SecretKey;
+    use vrf::openssl::{CipherSuite, ECVRF};
+    use vrf::VRF;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn same_seed_equals_same_random_8() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let mut vvrf2: VVRF = VVRF::new(message, sk);
+        let rn1 = vvrf1.generate_u8();
+        let rn2 = vvrf2.generate_u8();
+        println!("{:?}", rn1);
+        println!("{:?}", rn2);
+        assert_eq!(rn1, rn2);
+    }
+
+    #[test]
+    fn same_seed_equals_same_random_u16() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let mut vvrf2: VVRF = VVRF::new(message, sk);
+        let rn1 = vvrf1.generate_u16();
+        let rn2 = vvrf2.generate_u16();
+        println!("{:?}", rn1);
+        println!("{:?}", rn2);
+        assert_eq!(rn1, rn2);
+    }
+
+    #[test]
+    fn same_seed_equals_same_random_32() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let mut vvrf2: VVRF = VVRF::new(message, sk);
+        let rn1 = vvrf1.generate_u32();
+        let rn2 = vvrf2.generate_u32();
+        println!("{:?}", rn1);
+        println!("{:?}", rn2);
+        assert_eq!(rn1, rn2);
+    }
+    #[test]
+    fn same_seed_equals_same_random_u64() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let mut vvrf2: VVRF = VVRF::new(message, sk);
+        let rn1 = vvrf1.generate_u64();
+        let rn2 = vvrf2.generate_u64();
+        println!("{:?}", rn1);
+        println!("{:?}", rn2);
+        assert_eq!(rn1, rn2);
+    }
+
+    #[test]
+    fn same_seed_equals_same_random_u128() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let mut vvrf2: VVRF = VVRF::new(message, sk);
+        let rn1 = vvrf1.generate_u128();
+        let rn2 = vvrf2.generate_u128();
+        println!("{:?}", rn1);
+        println!("{:?}", rn2);
+        assert_eq!(rn1, rn2);
+    }
+
+    #[test]
+    fn hash_is_verifiable() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let vvrf: VVRF = VVRF::new(message, sk);
+        let mut vrf = ECVRF::from_suite(CipherSuite::SECP256K1_SHA256_TAI).unwrap();
+        let beta = vrf
+            .verify(&vvrf.get_pubkey(), &vvrf.get_proof(), &vvrf.get_message())
+            .unwrap();
+        let hash = vvrf.get_hash();
+        assert_eq!(hash.to_vec(), beta);
+    }
+
+    #[test]
+    fn generates_word_from_lib() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf: VVRF = VVRF::new(message, sk);
+        assert!(WORDS.contains(&(vvrf.generate_word()).as_str()));
+    }
+
+    #[test]
+    fn generates_right_num_words() {
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        assert_eq!((vvrf1.generate_words(7)).len(), 7);
+    }
+
+    #[test]
+    fn generates_rng_in_range(){
+        let sk = SecretKey::new(&mut rand::thread_rng());
+        let message = b"test";
+        let mut vvrf1: VVRF = VVRF::new(message, sk);
+        let rn = vvrf1.generate_u8_in_range(10, 100);
+        assert!(10 <= rn && rn <= 100);
+    }
+}
---
+++ vrrb_vrf/src/vrng.rs
@@ -0,0 +1,34 @@
+
+///trait VRNG defines method signatures for generating 
+///random numbers of all u types
+pub trait VRNG {
+    fn generate_u8(&mut self) -> u8;
+    fn generate_u16(&mut self) -> u16;
+    fn generate_u32(&mut self) -> u32;
+    fn generate_u64(&mut self) -> u64;
+    fn generate_u128(&mut self) -> u128;
+    fn generate_usize(&mut self) -> usize;
+    fn generate_word(&mut self) -> String;
+    fn generate_u8_in_range(&mut self, min: u8, max: u8) -> u8;
+    fn generate_u16_in_range(&mut self, min: u16, max: u16) -> u16;
+    fn generate_u32_in_range(&mut self, min: u32, max: u32) -> u32;
+    fn generate_u64_in_range(&mut self, min: u64, max: u64) -> u64;
+    fn generate_u128_in_range(&mut self, min: u128, max: u128) -> u128;
+    fn generate_usize_in_range(&mut self, min: usize, max: usize) -> usize;
+    fn generate_words(&mut self, n: usize) -> Vec<String> {
+        let mut vec: Vec<String> = Vec::new();
+        let mut i: usize = 0;
+        while i < n {
+            vec.push(self.generate_word());
+            i += 1;
+        }
+        vec
+    }
+    fn generate_phrase(&mut self, n: usize) -> String {
+        let vec: Vec<String> = self.generate_words(n);
+        let mut phrase: String = vec.join ("");
+        return phrase.trim_start().to_string();
+    }
+
+
+}
