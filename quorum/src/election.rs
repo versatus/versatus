d44a4204079b98ddba07cacf725fd64b07202d11
Anna <amouland@vrrb.io>
2022-08-21 12:09:44 -0400

    redoing quorum seed and pointer sum fxns

--- quorum/src/election.rs
+++ quorum/src/election.rs
@@ -4,9 +4,7 @@ use blockchain::blockchain::Blockchain;
 use state::state::Components;
 
 pub trait ELECTION{ 
-    fn generate_quorum_seed(&mut self, components: &Components) -> u64;
-    fn calculate_pointer_sums(&mut self, blockchain: &Blockchain) -> Vec<u64>;
-    fn elect_masternodes(&mut self, blockchain: &Blockchain) -> Vec<String>;
+    fn elect_quorum(&mut self, blockchain: &Blockchain) -> Vec<Staker>;
 }
 
  
