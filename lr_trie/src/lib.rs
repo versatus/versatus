196b32bddfd20d18b03a7dde1fc784b44eb21569
Daniel M <dmartinez@vrrb.io>
2022-08-23 15:13:09 +0200

    WIP add node def

--- lr_trie/src/lib.rs
+++ lr_trie/src/lib.rs
@@ -1,4 +1,4 @@
-/// This crate contains a left-right wrapped Merkle-Patricia Trie
+/// This crate contains a left-right wrapped, evmap-backed, Merkle-Patricia Trie
 /// heavily inspired by https://github.com/carver/eth-trie.rs which is a fork of https://github.com/citahub/cita-trie
 
 pub type Byte = u8;
