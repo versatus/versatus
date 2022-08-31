c324588a7809cd157a5eb81f1e3a5cd514a19036
Daniel M <dmartinez@vrrb.io>
2022-08-31 10:25:45 +0200

    fix tests

--- lrdb/src/lr_db.rs
+++ lrdb/src/lr_db.rs
@@ -10,7 +10,7 @@ pub type Nonce = u32;
 ///
 /// `WriteHandles` provides a way to gain write access to the database.
 /// `last_refresh` denotes the lastest `refresh` of the database.
-// #[allow(dead_code)]
+#[allow(dead_code)]
 pub struct LeftRightDatabase<K, V>
 where
     K: Clone + Eq + Hash + std::fmt::Debug,
@@ -49,65 +49,3 @@ where
         }
     }
 }
-
-// #[derive(Default, Debug, Clone)]
-#[derive(Clone)]
-// pub struct SyncDb<'a, K, V> {
-pub struct SyncDb<'a> {
-    db: Arc<LeftRightDatabase<&'a [u8], Vec<u8>>>,
-}
-
-impl<'a> SyncDb<'a> {
-    pub fn new() -> Self {
-        Self {
-            db: Arc::new(LeftRightDatabase::new()),
-        }
-    }
-}
-
-impl<'a> Default for SyncDb<'a> {
-    fn default() -> Self {
-        Self {
-            db: Arc::new(LeftRightDatabase::new()),
-        }
-    }
-}
-
-/*
-//
-// impl<K, V> Database for ArcWrap<LeftRightDatabase<K, V>>
-// impl<K, V> Database for LeftRightDatabase<K, V>
-// impl<K, V> Database for SyncDb<K, V>
-impl<'a> Database for SyncDb<'a>
-// where
-//     K: Clone + Eq + Hash + Send + Sync + std::fmt::Debug,
-//     V: Clone + Eq + evmap::ShallowCopy + Send + Sync + std::fmt::Debug,
-{
-    type Error = LeftRightDbError;
-
-    fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>> {
-        todo!()
-    }
-
-    fn insert(&self, key: &[u8], value: Vec<u8>) -> Result<()> {
-        todo!()
-    }
-
-    fn remove(&self, key: &[u8]) -> Result<()> {
-        todo!()
-    }
-
-    fn flush(&self) -> Result<()> {
-        todo!()
-    }
-
-    fn len(&self) -> Result<usize> {
-        todo!()
-    }
-
-    fn is_empty(&self) -> Result<bool> {
-        todo!()
-    }
-    //
-}
-*/
