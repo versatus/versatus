b64673d39ddadc9d46cd8be8042b996a0d2320e2
Daniel M <dmartinez@vrrb.io>
2022-09-01 13:50:25 +0200

    add basic typedefs

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub type NodeId = u16;
+pub type SecretKey = Vec<u8>;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
