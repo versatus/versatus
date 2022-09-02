e6ad506cebf0fa3961dce1de10c5727dfbda567a
Daniel M <dmartinez@vrrb.io>
2022-09-02 17:14:54 +0200

    WIP setup helpers

---
+++ service/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+/// Service is responsible for initializing the node, handling networking and config management
+struct Service {
+    //
+}
+
+impl Service {
+    pub fn start() {
+        //
+        // setup ports and control channels and loops
+    }
+}
+
+/// Main entrypoint
+fn main() -> Result<(), impl thiserror::Error> {
+    telemetry::TelemetrySubscriber::init(io::stdout)?;
+}
--- telemetry/src/lib.rs
+++ telemetry/src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use thiserror::Error;
+
+use tracing_subscriber::{
+    fmt::MakeWriter,
+    util::{SubscriberInitExt, TryInitError},
+};
+
+#[derive(Debug, Error)]
+pub enum TelemetryError {
+    #[error("failed to initialize: {0}")]
+    Init(#[from] TryInitError),
+
+    #[error("{0}")]
+    Other(String),
+
+    #[error("unknown error occurred")]
+    Unknown,
+}
+
+type Result<T> = std::result::Result<T, TelemetryError>;
+
+// TODO: figure out the proper generic sig
+#[derive(Debug)]
+pub struct TelemetrySubscriber
+// pub struct TelemetrySubscriber<S>
+where
+// S: Subscriber + SubscriberInitExt + Send + Sync,
+// S: Subscriber + SubscriberInitExt + Send + Sync,
+// S: Subscriber + Send + Sync,
+// W: for<'s> MakeWriter<'s> + 'static,
+// S: Subscriber + SubscriberInitExt + Send + Sync,
+// S: Subscriber + SubscriberInitExt,
+// S: Subscriber,
+// S: Subscriber<JsonFields, Format<Json>, LevelFilter, W>,
+{
+    // sub: S,
+}
+
+impl TelemetrySubscriber {
+    pub fn init<W>(out: W) -> Result<()>
+    where
+        W: for<'s> MakeWriter<'s> + 'static + Sync + Send,
+    {
+        let sub = tracing_subscriber::fmt()
+            .with_writer(out)
+            .with_file(true)
+            .with_line_number(true)
+            .with_thread_ids(true)
+            .with_thread_names(true)
+            .json()
+            .finish();
+
+        sub.try_init()?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
+
+    use std::{io::BufWriter, sync::Arc};
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let mut out = Vec::new();
+
+        let buf = Arc::new(BufWriter::new(out));
+
+        TelemetrySubscriber::init(buf).unwrap();
+
+        tracing::info!("hello world 2");
     }
 }
