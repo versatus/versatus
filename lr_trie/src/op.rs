2e9ed7c02ffa7a89fff1c71218fe4c89254d79f9
Daniel <dmartinez@vrrb.io>
2022-09-06 15:54:22 +0200

    Update LR Trie and its dependents to work with owned byte vectors instead of byte slices (#36)

--- lr_trie/src/op.rs
+++ lr_trie/src/op.rs
@@ -3,13 +3,14 @@ pub type Bytes = [Byte];
 
 #[derive(Debug)]
 #[non_exhaustive]
-pub enum Operation<'a> {
+pub enum Operation {
     /// Add a single value serialized to bytes
-    Add(&'a Bytes, &'a Bytes),
+    Add(Vec<u8>, Vec<u8>),
 
     /// Remove a value specified by the key from the trie
-    Remove(&'a Bytes),
+    Remove(Vec<u8>),
 
     /// Extend the state trie with the provided iterator over leaf values as byte slices.
-    Extend(Vec<(&'a Bytes, &'a Bytes)>),
+    // Extend(Vec<(&'a Bytes, &'a Bytes)>),
+    Extend(Vec<(Vec<u8>, Vec<u8>)>),
 }
