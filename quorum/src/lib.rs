c898d7bb453265f415932d0bddf5071ace46188b
Anna <anna.electros@gmail.com>
2022-09-12 06:59:50 -0400

    feat all tests passing, PR ready for approval

--- quorum/src/lib.rs
+++ quorum/src/lib.rs
@@ -1,24 +1,17 @@
-pub mod election;
+pub  mod election;
 pub mod quorum;
 
 #[cfg(test)]
 mod tests {
-    use std::{
-        collections::hash_map::DefaultHasher,
-        hash::{Hash, Hasher},
-    };
-
     use claim::claim::Claim;
-    use format_bytes::format_bytes;
-    use secp256k1::{
-        key::{PublicKey, SecretKey},
-        Secp256k1,
-    };
+    use crate::election::Election;
+    use crate::quorum::Quorum;
+    use secp256k1;
+    use secp256k1::{Secp256k1};
     use sha256::digest_bytes;
-    use vrrb_vrf::{vrng::VRNG, vvrf::VVRF};
-
-    use crate::{election::Election, quorum::Quorum};
-
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher}; 
+ 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -28,16 +21,18 @@ mod tests {
     fn not_enough_claims() {
         let mut dummyClaims: Vec<Claim> = Vec::new();
         let addr: String = "0x0000000000000000000000000000000000000000".to_string();
-        (0..3).for_each(|i| {
-            let secp = Secp256k1::new();
+        (0..3).for_each(
+            |i| {
+                let secp = Secp256k1::new();
 
-            let mut rng = rand::thread_rng();
-
-            let (secret_key, public_key) = secp.generate_keypair(&mut rng);
-            let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
-
-            dummyClaims.push(claim);
-        });
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
         let secp = Secp256k1::new();
 
         let mut rng = rand::thread_rng();
@@ -53,27 +48,105 @@ mod tests {
 
         let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
 
-        let payload = (10, 10, hash);
+        let payload1 = (10, 10, hash);
 
-        let mut quorum: Quorum = Quorum::new();
+        if let Ok(seed) = Quorum::generate_seed(payload1){
+            if let Ok(mut quorum) = Quorum::new(seed, 11, 11) {
+                assert!(quorum.run_election(dummyClaims).is_err());
+            };   
+        }
+    }
 
-        assert!(quorum.run_election(payload, dummyClaims).is_err());
+    
+    #[test]
+    fn invalid_seed_block_height() {
+        let mut dummyClaims: Vec<Claim> = Vec::new();
+        let addr: String = "0x0000000000000000000000000000000000000000".to_string();
+        (0..3).for_each(
+            |i| {
+                let secp = Secp256k1::new();
+
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
+        let secp = Secp256k1::new();
+
+        let mut rng = rand::thread_rng();
+
+        let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+
+        let mut hasher = DefaultHasher::new();
+        public_key.hash(&mut hasher);
+        let pubkey_hash = hasher.finish();
+
+        let mut pub_key_bytes = pubkey_hash.to_string().as_bytes().to_vec();
+        pub_key_bytes.push(1u8);
+
+        let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
+
+        let payload1 = (10, 0, hash);
+
+        assert!(Quorum::generate_seed(payload1).is_err());     
+    }
+
+    
+    #[test]
+    fn invalid_seed_block_timestamp() {
+        let mut dummyClaims: Vec<Claim> = Vec::new();
+        let addr: String = "0x0000000000000000000000000000000000000000".to_string();
+        (0..3).for_each(
+            |i| {
+                let secp = Secp256k1::new();
+
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
+        let secp = Secp256k1::new();
+
+        let mut rng = rand::thread_rng();
+
+        let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+
+        let mut hasher = DefaultHasher::new();
+        public_key.hash(&mut hasher);
+        let pubkey_hash = hasher.finish();
+
+        let mut pub_key_bytes = pubkey_hash.to_string().as_bytes().to_vec();
+        pub_key_bytes.push(1u8);
+
+        let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
+
+        let payload1 = (0, 10, hash);
+
+        assert!(Quorum::generate_seed(payload1).is_err());     
     }
 
     #[test]
-    fn invalid_block_height() {
+    fn invalid_election_block_height() {
         let mut dummyClaims: Vec<Claim> = Vec::new();
         let addr: String = "0x0000000000000000000000000000000000000000".to_string();
-        (0..20).for_each(|i| {
-            let secp = Secp256k1::new();
+        (0..3).for_each(
+            |i| {
+                let secp = Secp256k1::new();
 
-            let mut rng = rand::thread_rng();
-
-            let (secret_key, public_key) = secp.generate_keypair(&mut rng);
-            let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
-
-            dummyClaims.push(claim);
-        });
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
         let secp = Secp256k1::new();
 
         let mut rng = rand::thread_rng();
@@ -89,27 +162,32 @@ mod tests {
 
         let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
 
-        let payload = (10, 0, hash);
+        let payload1 = (10, 10, hash);
 
-        let mut quorum: Quorum = Quorum::new();
+        let seed = Quorum::generate_seed(payload1);
 
-        assert!(quorum.run_election(payload, dummyClaims).is_err());
+        let mut quorum: Quorum;
+        if let Ok(seed) = seed{
+            assert!(Quorum::new(seed, 11, 0).is_err());
+        }   
     }
 
     #[test]
-    fn invalid_block_timestamp() {
+    fn invalid_election_block_timestamp() {
         let mut dummyClaims: Vec<Claim> = Vec::new();
         let addr: String = "0x0000000000000000000000000000000000000000".to_string();
-        (0..25).for_each(|i| {
-            let secp = Secp256k1::new();
+        (0..20).for_each(
+            |i| {
+                let secp = Secp256k1::new();
 
-            let mut rng = rand::thread_rng();
-
-            let (secret_key, public_key) = secp.generate_keypair(&mut rng);
-            let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
-
-            dummyClaims.push(claim);
-        });
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
         let secp = Secp256k1::new();
 
         let mut rng = rand::thread_rng();
@@ -125,27 +203,29 @@ mod tests {
 
         let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
 
-        let payload = (0, 10, hash);
+        let payload1 = (10, 10, hash);
 
-        let mut quorum: Quorum = Quorum::new();
-
-        assert!(quorum.run_election(payload, dummyClaims).is_err());
+        if let Ok(seed) = Quorum::generate_seed(payload1){
+            assert!(Quorum::new(seed, 0, 11).is_err());    
+        }
     }
 
     #[test]
     fn elect_quorum() {
         let mut dummyClaims: Vec<Claim> = Vec::new();
         let addr: String = "0x0000000000000000000000000000000000000000".to_string();
-        (0..25).for_each(|i| {
-            let secp = Secp256k1::new();
+        (0..25).for_each(
+            |i| {
+                let secp = Secp256k1::new();
 
-            let mut rng = rand::thread_rng();
-
-            let (secret_key, public_key) = secp.generate_keypair(&mut rng);
-            let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
-
-            dummyClaims.push(claim);
-        });
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims.push(claim);
+            }
+        );
         let secp = Secp256k1::new();
 
         let mut rng = rand::thread_rng();
@@ -161,11 +241,62 @@ mod tests {
 
         let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
 
+        let payload1 = (10, 10, hash);
+
+        if let Ok(seed) = Quorum::generate_seed(payload1){
+            if let Ok(mut quorum) = Quorum::new(seed, 11, 11) {
+                quorum.run_election(dummyClaims);
+                assert!(quorum.master_pubkeys.len() == 13);
+            };   
+        }
+    }
+    
+    #[test] 
+    fn elect_identical_quorums() {
+        let mut dummyClaims1: Vec<Claim> = Vec::new();
+        let mut dummyClaims2: Vec<Claim> = Vec::new();
+        let addr: String = "0x0000000000000000000000000000000000000000".to_string();
+        (0..3).for_each(
+            |i| {
+                let secp = Secp256k1::new();
+
+                let mut rng = rand::thread_rng();
+        
+                let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+                let claim: Claim = Claim::new(public_key.to_string(), addr.clone(), i as u128);
+            
+                dummyClaims1.push(claim.clone());
+                dummyClaims2.push(claim.clone());
+            }
+        );
+
+        let secp = Secp256k1::new();
+
+        let mut rng = rand::thread_rng();
+
+        let (secret_key, public_key) = secp.generate_keypair(&mut rng);
+
+        let mut hasher = DefaultHasher::new();
+        public_key.hash(&mut hasher);
+        let pubkey_hash = hasher.finish();
+
+        let mut pub_key_bytes = pubkey_hash.to_string().as_bytes().to_vec();
+        pub_key_bytes.push(1u8);
+        
+        let hash = digest_bytes(digest_bytes(&pub_key_bytes).as_bytes());
+        
         let payload = (10, 10, hash);
 
-        let mut quorum: Quorum = Quorum::new();
-        quorum.run_election(payload, dummyClaims);
-
-        assert!(quorum.master_pubkeys.len() == 13);
-    }
+        if let Ok(seed1) = Quorum::generate_seed(payload.clone()) {
+            if let Ok(seed2) = Quorum::generate_seed(payload.clone()){
+                if let Ok(mut quorum1) = Quorum::new(seed1, 11, 11) {
+                    if let Ok(mut quorum2) = Quorum::new(seed2, 11, 11) {
+                        quorum1.run_election(dummyClaims1);
+                        quorum2.run_election(dummyClaims2);
+                        assert!(quorum1.master_pubkeys == quorum2.master_pubkeys);
+                    }
+                }
+            }
+        }
+    } 
 }
--- quorum/src/quorum.rs
+++ quorum/src/quorum.rs
@@ -1,167 +1,168 @@
-use std::u32::MAX as u32MAX;
 
+use crate::election:: Election;
+use vrrb_vrf::{vvrf::VVRF, vrng::VRNG};
+use std::u32::MAX as u32MAX;
 use claim::claim::Claim;
 use thiserror::Error;
-use vrrb_vrf::{vrng::VRNG, vvrf::VVRF};
 
-use crate::election::Election;
-
+///Error type for Quorum
 #[derive(Error, Debug)]
-pub enum InvalidQuorum {
-    #[error("inavlid seed generated")]
-    InvalidSeedError(),
+pub enum InvalidQuorum{
+  #[error("inavlid seed generated")]
+   InvalidSeedError(), 
 
-    #[error("invalid pointer sum")]
-    InvalidPointerSumError(Vec<Claim>),
+  #[error("invalid pointer sum")]
+   InvalidPointerSumError(Vec<Claim>),
 
-    #[error("invalid child block")]
-    InvalidChildBlockError(),
+  #[error("invalid child block")]
+   InvalidChildBlockError(),
 
-    #[error("not enough eligible nodes")]
-    InsufficientNodesError(),
+   #[error("not enough eligible nodes")]
+   InsufficientNodesError(),
+
+   #[error("quorum does not contain a seed")]
+   NoSeedError(),
 }
 
-pub struct Quorum {
-    pub quorum_seed: u128,
-    pub master_pubkeys: Vec<String>,
-    pub quorum_pk: String,
-    pub election_block_height: u128,
-    pub election_timestamp: u128,
+///Quorum struct which is created and modified when an election is run
+pub struct Quorum{
+  pub quorum_seed: u64,
+  pub master_pubkeys: Vec<String>,
+  pub quorum_pk: String,
+  pub election_block_height: u128,
+  pub election_timestamp: u128,
 }
 
+///generic types from Election trait defined here for Quorums
 type Timestamp = u128;
 type Height = u128;
 type BlockHash = String;
+type Seed = u64;
 
-impl Election for Quorum {
-    type Ballot = Vec<Claim>;
-    type Error = InvalidQuorum;
-    type Payload = (Timestamp, Height, BlockHash);
-    type Return = Self;
+///Payload data comes from current child block
+impl Election for Quorum{
+   type Return = Self;
+   type Error = InvalidQuorum;
+   type Ballot = Vec<Claim>;
+   type Payload = (Timestamp, Height, BlockHash);
+   type Seed = Seed;
 
-    fn elect_quorum(
-        &mut self,
-        payload: Self::Payload,
-        ballot: Self::Ballot,
-    ) -> Result<&Self::Return, Self::Error> {
-        let quorum_seed = match self.generate_quorum_seed(payload.0, payload.1, payload.2) {
-            Ok(quorum_seed) => quorum_seed,
-            Err(e) => return Err(e),
-        };
-        self.quorum_seed = quorum_seed;
+   ///a miner calls this fxn to generate a u64 seed for the election using the vrrb_vrf crate
+   fn generate_seed(payload: Self::Payload) -> Result<Seed, InvalidQuorum> {
+      
+      if !Quorum::check_payload_validity(payload.1, payload.0){
+         return Err(InvalidQuorum::InvalidChildBlockError());
+      }
+     
+      let sk = VVRF::generate_secret_key();
+      let mut vvrf = VVRF::new((payload.2).as_bytes(), sk);
+   
+      if VVRF::verify_seed(&mut vvrf).is_err() {
+         return Err(InvalidQuorum::InvalidSeedError());
+      }
+      
+      let mut random_number = vvrf.generate_u64(); 
+      while random_number < u32MAX as u64 { 
+         random_number = vvrf.generate_u64(); 
+      }
+      return Ok(random_number);
+   }
 
-        let eligible_claims = match Quorum::get_eligible_claims(ballot) {
-            Ok(eligible_claims) => eligible_claims,
-            Err(e) => return Err(e),
-        };
+   ///master nodes run elections to determine the next master node quorum
+  fn run_election(&mut self, ballot: Self::Ballot) -> Result<&Self::Return, Self::Error>{
 
-        let elected_quorum = match self.get_final_quorum(quorum_seed, eligible_claims) {
-            Ok(elected_quorum) => elected_quorum,
-            Err(e) => return Err(e),
-        };
-        return Ok(elected_quorum);
-    }
+      if self.election_block_height == 0 || self.election_timestamp == 0{
+         return Err(InvalidQuorum::InvalidChildBlockError());
+      }
+      
+      let eligible_claims = match Quorum::get_eligible_claims(ballot){
+         Ok(eligible_claims) => eligible_claims,
+         Err(e) => return Err(e),
+      };
 
-    fn run_election(
-        &mut self,
-        payload: Self::Payload,
-        ballot: Self::Ballot,
-    ) -> Result<&Self::Return, Self::Error> {
-        match self.elect_quorum(payload, ballot) {
-            Ok(quorum) => return Ok(quorum),
-            Err(e) => return Err(e),
-        };
-    }
+      let elected_quorum = match self.get_final_quorum(eligible_claims){
+         Ok(elected_quorum) => elected_quorum,
+         Err(e) => return Err(e),
+      };
+
+      return Ok(elected_quorum);
+   }
 }
 
-//result enum for errors
-impl Quorum {
-    //make new generate a blank/default quorum like a constructor
-    pub fn new() -> Quorum {
-        return Quorum {
-            quorum_seed: 0,
+
+impl Quorum{
+  ///makes a new Quorum and initializes seed, child block height, and child block timestamp
+   pub fn new(seed: u64, timestamp: u128, height: u128) -> Result<Quorum, InvalidQuorum> {
+      if !Quorum::check_payload_validity(height, timestamp){
+         return Err(InvalidQuorum::InvalidChildBlockError());
+      } else {
+         return Ok(Quorum{
+            quorum_seed: seed,
             master_pubkeys: Vec::new(),
             quorum_pk: String::new(),
-            election_block_height: 0,
-            election_timestamp: 0,
-        };
-    }
+            election_block_height: height,
+            election_timestamp: timestamp,
+         });
+      }
+   }
 
-    pub fn generate_quorum_seed(
-        &mut self,
-        timestamp: Timestamp,
-        height: Height,
-        block_hash: BlockHash,
-    ) -> Result<u128, InvalidQuorum> {
-        if height == 0 {
-            return Err(InvalidQuorum::InvalidChildBlockError());
-        } else if timestamp == 0 {
-            return Err(InvalidQuorum::InvalidChildBlockError());
-        } else {
-            let sk = VVRF::generate_secret_key();
-            let mut vvrf = VVRF::new(block_hash.as_bytes(), sk);
+   ///checks if the child block height and timestamp are valid
+   ///used at seed and quorum creation
+   pub fn check_payload_validity(timestamp: Timestamp, height: Height) -> bool {
+      if height == 0 || timestamp == 0 {
+         return false;
+      } else {
+         return true;
+      }
+   }
 
-            if VVRF::verify_seed(&mut vvrf).is_err() {
-                return Err(InvalidQuorum::InvalidSeedError());
-            }
-
-            let mut random_number = vvrf.generate_u64();
-            while random_number < u32MAX as u64 {
-                random_number = vvrf.generate_u64();
-            }
-
-            self.quorum_seed = random_number as u128;
-            self.election_timestamp = timestamp;
-            self.election_block_height = height;
-
-            return Ok(random_number as u128);
+   ///gets all claims that belong to eligible nodes (master nodes)
+   /// needs to be modifed as claim field eligible:  bool needs to become a uX of staked amt
+   pub fn get_eligible_claims(claims: Vec<Claim>) -> Result<Vec<Claim>, InvalidQuorum> {
+     let mut eligible_claims = Vec::<Claim>::new();
+     claims.into_iter().filter(|claim| claim.eligible).for_each(
+        |claim| {
+           eligible_claims.push(claim);
         }
-    }
+     );
+     if eligible_claims.len() < 20 {
+        return Err(InvalidQuorum::InsufficientNodesError());
+     }
+     
+     let eligible_claims = eligible_claims;
+     Ok(eligible_claims) 
+   }
 
-    pub fn get_eligible_claims(claims: Vec<Claim>) -> Result<Vec<Claim>, InvalidQuorum> {
-        let mut eligible_claims = Vec::<Claim>::new();
-        claims
-            .into_iter()
-            .filter(|claim| claim.eligible == true)
-            .for_each(|claim| {
-                eligible_claims.push(claim.clone());
-            });
+   ///gets the final quorum by getting 51% of master nodes with lowest pointer sums
+   pub fn get_final_quorum(
+     &mut self, 
+     claims: Vec<Claim>) -> Result<&Quorum, InvalidQuorum> {
 
-        if eligible_claims.len() < 20 {
-            return Err(InvalidQuorum::InsufficientNodesError());
-        }
-        let eligible_claims = eligible_claims;
-        return Ok(eligible_claims);
-    }
+      if self.quorum_seed == 0 {
+         return Err(InvalidQuorum::NoSeedError());
+      }
 
-    pub fn get_final_quorum(
-        &mut self,
-        quorum_seed: u128,
-        claims: Vec<Claim>,
-    ) -> Result<&Quorum, InvalidQuorum> {
-        let num_claims = ((claims.len() as f32) * 0.51).ceil() as usize;
+     let num_claims =((claims.len() as f32)* 0.51).ceil() as usize;
 
-        let mut claim_tuples: Vec<(Option<u128>, &String)> = claims
-            .iter()
-            .filter(|claim| claim.get_pointer(quorum_seed) != None)
-            .map(|claim| (claim.get_pointer(quorum_seed), &claim.pubkey))
-            .collect();
+     let mut claim_tuples: Vec<(u128, &String)> = claims.iter().filter( 
+        |claim| claim.get_pointer(self.quorum_seed as u128) != None).map(
+        |claim| (claim.get_pointer(self.quorum_seed as u128).unwrap(), &claim.pubkey)
+     ).collect();
 
-        if claim_tuples.len() < 20 {
-            return Err(InvalidQuorum::InvalidPointerSumError(claims));
-        }
 
-        claim_tuples.sort_by_key(|claim_tuple| claim_tuple.0.unwrap());
+      if claim_tuples.len() < 20 {
+      return Err(InvalidQuorum::InvalidPointerSumError(claims));
+     }
 
-        let pubkeys: Vec<String> = claim_tuples
-            .into_iter()
-            .map(|claim_tuple| claim_tuple.1.clone())
-            .take(num_claims)
-            .collect();
+     claim_tuples.sort_by_key(|claim_tuple| claim_tuple.0);
 
-        let final_pubkeys = Vec::from_iter(pubkeys[0..num_claims].iter().cloned());
-        self.master_pubkeys = final_pubkeys;
+     let pubkeys: Vec<String> = claim_tuples.into_iter().map(
+        |claim_tuple| claim_tuple.1.clone()
+     ).take(num_claims).collect();
 
-        return Ok(self);
-    }
+     let final_pubkeys = Vec::from_iter(pubkeys[0..num_claims].iter().cloned());
+     self.master_pubkeys = final_pubkeys;
+
+     Ok(self)
+   }
 }
