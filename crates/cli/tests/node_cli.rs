fc82523ac971a393d8d1379c8344170a0b01fd94
Daniel M <dmartinez@vrrb.io>
2022-10-07 16:13:30 +0200

    fix cli tests

--- crates/cli/tests/node_cli.rs
+++ crates/cli/tests/node_cli.rs
@@ -1,13 +1,13 @@
 use assert_cmd::Command;
 
 #[tokio::test]
-#[ignore = "temporarily broken because of GH actions issue"]
+// #[ignore = "temporarily broken because of GH actions issue"]
 pub async fn cli_should_show_help_text() {
-    let mut cmd = Command::cargo_bin("vrrb").unwrap();
+    let mut cmd = Command::cargo_bin("cli").unwrap();
     let help_text = r#"cli 0.0.0
 
 USAGE:
-    vrrb [OPTIONS] [SUBCOMMAND]
+    cli [OPTIONS] [SUBCOMMAND]
 
 OPTIONS:
     -c, --config <FILE>    Sets a custom config file
