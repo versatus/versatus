ed24100d17820f185187914ddcaa69908b7afb88
Daniel M <dmartinez@vrrb.io>
2022-10-10 13:06:24 +0200

    add kitchensink core crate

---
+++ crates/core/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod command_router;
+pub mod result;
+pub use result::*;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
