b807bd1d956e0dcc077436c0ea82148960c84107
Daniel <dmartinez@vrrb.io>
2023-01-10 12:02:43 +0100

    Update mempool architecture to accept new transaction structure (#128)

--- crates/mempool/src/error.rs
+++ crates/mempool/src/error.rs
@@ -1,6 +1,17 @@
-#[derive(PartialEq, Eq, Debug)]
+use primitives::TxHashString;
+
+#[derive(thiserror::Error, PartialEq, Eq, Debug)]
 pub enum MempoolError {
-    TransactionMissing,
-    TransactionInvalid,
-    TransactionExists,
+    #[error("transaction {0} was not found in mempool")]
+    #[deprecated]
+    TransactionMissing(TxHashString),
+
+    #[error("transaction {0} was not found in mempool")]
+    TransactionNotFound(TxHashString),
+
+    #[error("invalid transaction {0}")]
+    TransactionInvalid(TxHashString),
+
+    #[error("transaction {0} already exists")]
+    TransactionExists(TxHashString),
 }
--- crates/validator/src/txn_validator.rs
+++ crates/validator/src/txn_validator.rs
@@ -5,9 +5,9 @@ use std::{
 
 use left_right::ReadHandle;
 use lr_trie::{GetDeserialized, LeftRightTrieError};
-use lrdb::Account;
 use patriecia::{db::Database, error::TrieError, inner::InnerTrie};
 use vrrb_core::{
+    account::Account,
     keypair::{KeyPair, MinerPk},
     txn::Txn,
 };
