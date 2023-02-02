686f77eee4477b4faa8e3b771ac6d3bf37d92d64
Daniel M <dmartinez@vrrb.io>
2023-02-02 01:46:09 +0100

    rm commented code

--- crates/node/src/node.rs
+++ crates/node/src/node.rs
@@ -116,63 +116,6 @@ impl Node {
             event_router.subscribe(&Topic::Transactions)?,
         )?;
 
-
-        // info!("Testing RAPTOR Q");
-        // {
-        //     let mut b1 = BroadcastEngine::new(config.raptorq_gossip_address.port(),
-        // 10)         .await
-        //         .unwrap();
-        //
-        //     let c = b1
-        //         .add_raptor_peers(vec![
-        //             "127.0.0.1:5002".parse().unwrap(),
-        //             "127.0.0.1:5003".parse().unwrap(),
-        //             "127.0.0.1:5004".parse().unwrap(),
-        //         ])
-        //         .await;
-        //
-        //     if config.raptorq_gossip_address.port() == 5001 {
-        //         let d = read_file(PathBuf::from(
-        //             "/Users/vinay10949/Projects/vrrb/crates/node/src/t.txt",
-        //         ));
-        //         if let Ok(data) = String::from_utf8(d) {
-        //             let data = data.trim_end_matches('\0').to_string().replace("\\",
-        // "");             let txn: Txn = serde_json::from_str(&data).unwrap();
-        //             let data = RaptorBroadCastedData::Txn(txn);
-        //             let data = serde_json::to_string(&data).unwrap();
-        //             let broadcast_status = b1
-        //                 .unreliable_broadcast(
-        //                     data.as_bytes().to_vec(),
-        //                     3000,
-        //                     config.raptorq_gossip_address.port(),
-        //                 )
-        //                 .await;
-        //             info!("Broadcasting Status :{:?}", broadcast_status);
-        //         }
-        //     } else {
-        //         let (batch_sender, batch_receiver) =
-        // unbounded::<RaptorBroadCastedData>();         thread::spawn(move ||
-        // loop {             if let Ok(data) = batch_receiver.recv() {
-        //                 info!("Txn Received {:?}", data);
-        //             }
-        //         });
-        //         let _ = b1
-        //             .process_received_packets(config.raptorq_gossip_address.port(),
-        // batch_sender)             .await;
-        //     }
-        // }
-
-        // pub fn read_file(file_path: PathBuf) -> Vec<u8> {
-        //     let metadata = std::fs::metadata(&file_path).expect("unable to read
-        // metadata");     let mut buffer = vec![0; metadata.len() as usize];
-        //     std::fs::File::open(file_path)
-        //         .unwrap()
-        //         .read_exact(&mut buffer)
-        //         .expect("buffer overflow");
-        //     buffer
-        // }
-
-
         // TODO: report error from handle
         let event_router_handle =
             tokio::spawn(async move { event_router.start(&mut events_rx).await });
