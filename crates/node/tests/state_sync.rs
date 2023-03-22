147a6924966c0f631bfdfcbd4c0cbe829c3dcff3
Daniel M <dmartinez@vrrb.io>
2023-03-22 22:20:04 +0100

    wip update

--- crates/node/tests/state_sync.rs
+++ crates/node/tests/state_sync.rs
@@ -5,21 +5,25 @@ use std::{
 
 use jsonrpsee::{core::client::Subscription, ws_client::WsClientBuilder};
 use node::{test_utils::create_mock_full_node_config, Node, NodeType, RuntimeModuleState};
+use primitives::generate_account_keypair;
+use secp256k1::Message;
 use tokio::sync::mpsc::unbounded_channel;
 use vrrb_config::NodeConfig;
-use vrrb_core::event_router::Event;
+use vrrb_core::{event_router::Event, txn::NewTxnArgs};
 use vrrb_rpc::rpc::{api::RpcApiClient, client::create_client};
 
 #[tokio::test]
-#[ignore]
 async fn nodes_can_synchronize_state() {
-    let node_config = create_mock_full_node_config();
+    // NOTE: two instances of a config are required because the node will create a
+    // data directory for the database which cannot be the same for both nodes
+    let node_config_1 = create_mock_full_node_config();
+    let node_config_2 = create_mock_full_node_config();
 
     let (ctrl_tx_1, ctrl_rx_1) = unbounded_channel::<Event>();
     let (ctrl_tx_2, ctrl_rx_2) = unbounded_channel::<Event>();
 
-    let vrrb_node_1 = Node::start(&node_config, ctrl_rx_1).await.unwrap();
-    let vrrb_node_2 = Node::start(&node_config, ctrl_rx_2).await.unwrap();
+    let vrrb_node_1 = Node::start(&node_config_1, ctrl_rx_1).await.unwrap();
+    let vrrb_node_2 = Node::start(&node_config_2, ctrl_rx_2).await.unwrap();
 
     let client = create_client(vrrb_node_1.jsonrpc_server_address())
         .await
@@ -35,6 +39,32 @@ async fn nodes_can_synchronize_state() {
     let handle_2 = tokio::spawn(async move {
         vrrb_node_2.wait().await.unwrap();
     });
+
+    for _ in 0..1_00 {
+        let (sk, pk) = generate_account_keypair();
+
+        let signature =
+            sk.sign_ecdsa(Message::from_hashed_data::<secp256k1::hashes::sha256::Hash>(b"vrrb"));
+
+        client
+            .create_txn(NewTxnArgs {
+                timestamp: 0,
+                sender_address: String::from("mock sender_address"),
+                sender_public_key: pk,
+                receiver_address: String::from("mock receiver_address"),
+                token: None,
+                amount: 0,
+                signature,
+                nonce: 0,
+                validators: None,
+            })
+            .await
+            .unwrap();
+    }
+
+    let mempool_snapshot = client.get_full_mempool().await.unwrap();
+
+    assert!(!mempool_snapshot.is_empty());
 
     ctrl_tx_1.send(Event::Stop).unwrap();
     ctrl_tx_2.send(Event::Stop).unwrap();
