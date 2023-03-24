959573ce4b415e3e189daddc199021c93f7e4a01
berryhill <matthewberryhill@gmail.com>
2023-03-23 17:51:22 -0700

    add grpc client

--- crates/grpc/src/client.rs
+++ crates/grpc/src/client.rs
@@ -1,1 +1,34 @@
+use hello_world::{greeter_client::GreeterClient, HelloRequest};
+use tonic::transport::{Certificate, Channel, ClientTlsConfig};
 
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data_dir = std::path::PathBuf::from_iter([std::env!("CARGO_MANIFEST_DIR"), "data"]);
+    let pem = std::fs::read_to_string(data_dir.join("tls/ca.pem"))?;
+    let ca = Certificate::from_pem(pem);
+
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(ca)
+        .domain_name("example.com");
+
+    let channel = Channel::from_static("http://127.0.0.1:50051")
+        .tls_config(tls)?
+        .connect()
+        .await?;
+
+    let mut client = GreeterClient::new(channel);
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
