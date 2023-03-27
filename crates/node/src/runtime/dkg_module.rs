1dd7545536b127a037eff719cd07c5157f2f1747
Daniel M <dmartinez@vrrb.io>
2023-03-27 22:54:50 +0200

    run formatter

--- crates/node/src/runtime/dkg_module.rs
+++ crates/node/src/runtime/dkg_module.rs
@@ -13,6 +13,7 @@ use dkg_engine::{
     dkg::DkgGenerator,
     types::{config::ThresholdConfig, DkgEngine, DkgError, DkgResult},
 };
+use events::{DirectedEvent, Event, SyncPeerData, Topic};
 use hbbft::{crypto::PublicKey, sync_key_gen::Part};
 use kademlia_dht::{Key, Node, NodeData};
 use laminar::{Config, ErrorKind, Packet, Socket, SocketEvent};
@@ -35,7 +36,6 @@ use serde::{Deserialize, Serialize};
 use telemetry::info;
 use theater::{Actor, ActorId, ActorLabel, ActorState, Handler};
 use tracing::error;
-use events::{DirectedEvent, Event, SyncPeerData, Topic};
 
 use crate::{result::Result, NodeError};
 
