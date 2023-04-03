8acb6e85b811159aeb49d55688e1f1fbce0dc9ff
Andrew N. Smith <asmith@vrrb.io>
2023-04-03 17:10:44 -0400

    add recursive rounds based lookup of convergence and orphaned proposal blocks to the <Miner as BlockBuilder> trait.

--- crates/miner/src/block_builder.rs
+++ crates/miner/src/block_builder.rs
@@ -1,3 +1,8 @@
+use std::collections::HashSet;
+
+use block::{RefHash, Block};
+use bulldag::vertex::Vertex;
+
 use crate::conflict_resolver::Resolver;
 /// A trait that can be implemented on any type that can build blocks.
 /// For current purposes, this is to be implemented on both Miner struct 
@@ -24,4 +29,28 @@ pub trait BlockBuilder: Resolver {
     fn update(&mut self, adjustment: &i128); 
     fn build(&self) -> Option<Self::BlockType>;
     fn get_references(&self) -> Option<Vec<Self::RefType>>;
+
+    fn get_orphaned_references(
+        &self, 
+        idx: RefHash, 
+        current_round: usize, 
+        n_rounds: usize
+    ) -> Vec<Self::RefType> {
+        let _ = n_rounds;
+        let _ = current_round;
+        let _ = idx;
+        vec![]
+    }
+
+    fn get_last_block_vertex(&self, idx: Option<RefHash>) -> Option<Vertex<Block, String>> {
+        let _ = idx;
+        None
+    }
+
+    fn get_n_rounds_convergence(&self, idx: RefHash, current_round: usize, n_rounds: usize) -> HashSet<RefHash> {
+        let _ = idx;
+        let _ = current_round;
+        let _ = n_rounds;
+        vec![]
+    }
 }
