79995df0e29c904c7fe258b84c1a95423da86017
Andrew N. Smith <asmith@owlshares.com>
2023-04-11 15:19:32 -0400

    fix conflict with election module exposure in runtime/mod.rs

--- crates/node/src/runtime/mod.rs
+++ crates/node/src/runtime/mod.rs
@@ -54,7 +54,6 @@ pub mod mining_module;
 pub mod reputation_module;
 pub mod state_module;
 pub mod swarm_module;
-pub mod election_module;
 pub mod dag_module;
 
 pub async fn setup_runtime_components(
