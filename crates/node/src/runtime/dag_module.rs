185373c61203a563fd03e3728459f5471753df22
Daniel M <dmartinez@vrrb.io>
2023-04-18 02:45:07 +0200

    uncomment broken code

--- crates/node/src/runtime/dag_module.rs
+++ crates/node/src/runtime/dag_module.rs
@@ -356,6 +356,7 @@ impl Handler<EventMessage> for DagModule {
                 }
             },
             Event::NoOp => {},
+            // _ => telemetry::warn!("unrecognized command received: {:?}", event),
             _ => {},
         }
         Ok(ActorState::Running)
