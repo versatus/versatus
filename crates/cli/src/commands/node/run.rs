225846001e087dc1a55dde1324bfab1986454938
Daniel M <dmartinez@vrrb.io>
2023-06-02 09:03:53 +0200

    rm commented code

--- crates/cli/src/commands/node/run.rs
+++ crates/cli/src/commands/node/run.rs
@@ -301,7 +301,6 @@ pub async fn run(args: RunOpts) -> Result<()> {
 async fn run_blocking(node_config: NodeConfig) -> Result<()> {
     let (ctrl_tx, ctrl_rx) = tokio::sync::mpsc::unbounded_channel::<Event>();
 
-    // let vrrb_node = Node::start(&node_config, ctrl_rx)
     let mut vrrb_node = Node::start(&node_config)
         .await
         .map_err(|_| CliError::Other(String::from("failed to listen for ctrl+c")))?;
