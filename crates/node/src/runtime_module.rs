29157cb5825d9dc746eb2e40bf768de4fee28c67
Daniel <dmartinez@vrrb.io>
2023-06-12 17:17:15 +0200

    Add RuntimeComponent abstraction (#370)

--- crates/node/src/runtime_module.rs
+++ crates/node/src/runtime_module.rs
@@ -1,23 +1,10 @@
-use async_trait::async_trait;
-use events::Event;
-use tokio::sync::broadcast::Receiver;
-
-use crate::result::Result;
+#[deprecated(note = "Use NodeState directly instead")]
+pub type RuntimeModuleState = NodeState;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub enum RuntimeModuleState {
+pub enum NodeState {
     Starting,
     Running,
     Stopped,
     Terminating,
-}
-
-/// RuntimeModule represents a node component that is loaded on startup and
-/// controls whenever a node is terminated
-#[async_trait]
-#[deprecated]
-pub trait RuntimeModule {
-    fn name(&self) -> String;
-    fn status(&self) -> RuntimeModuleState;
-    async fn start(&mut self, events_rx: &mut Receiver<Event>) -> Result<()>;
 }
