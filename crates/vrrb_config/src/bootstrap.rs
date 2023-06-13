125212d302a61763cd18f1e39eced613ecb89cf6
Daniel M <dmartinez@vrrb.io>
2023-06-13 17:39:20 +0200

    update bootstrap config

--- crates/vrrb_config/src/bootstrap.rs
+++ crates/vrrb_config/src/bootstrap.rs
@@ -1,6 +1,21 @@
-use serde::Deserialize;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-#[derive(Debug, Default, Clone, Deserialize)]
+use primitives::NodeId;
+use serde::Deserialize;
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct BootstrapConfig {
-    //
+    pub id: NodeId,
+    pub addr: SocketAddr,
+}
+
+impl Default for BootstrapConfig {
+    fn default() -> Self {
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 0);
+        BootstrapConfig {
+            id: Uuid::nil().to_string(),
+            addr,
+        }
+    }
 }
