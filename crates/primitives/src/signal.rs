e8b402e660ac89c205540f1da46aab9951657b60
Chris O'Brien <obrchr96@gmail.com>
2023-06-16 10:16:28 -0500

    update signals docstrings

--- crates/primitives/src/signal.rs
+++ crates/primitives/src/signal.rs
@@ -3,16 +3,13 @@ use std::time::Duration;
 #[derive(Clone, Debug, Default)]
 pub struct StopSignal;
 
-/// The duration of time, in seconds, a potential peer must wait in order to
+/// The duration of time, in seconds, potential peers must wait in order to
 /// register in the [Event::GeneratePayloadForPeerRegistration]. The interval
-/// at which peers can register to join a neighboring quorum pool.
+/// at which register requests can be made.
 pub const REGISTER_REQUEST: Duration = Duration::new(60, 0);
-/// The duration of time, in seconds, a potential peer must wait before they can
+/// The duration of time, in seconds, potential peers must wait before they can
 /// be validated in the [Event::InitiateSyncPeers].
-/// The interval at which potential peers can be evaluated as they attempt to
-/// join the neighboring quorum pool.
 pub const RETRIEVE_PEERS_REQUEST: Duration = Duration::new(30, 0);
 /// The duration of time, in seconds, before qualified peer namespaces can be
-/// pulled from the neighboring quorum pool in the
-/// [Event::PullFarmerNamespaces].
+/// pulled from the rendezvous nodes in the [Event::PullFarmerNamespaces].
 pub const SYNC_FARMER_NAMESPACES_REQUEST: Duration = Duration::new(60, 0);
