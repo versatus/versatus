59c01dda82610aff1d00e517bd12c6063a29e259
Chris-san <57543709+eureka-cpu@users.noreply.github.com>
2023-06-21 10:32:42 -0500

    add rustup run nightly to cargo invocation in pre-commit script (#383)
    Fixes warnings that occur due to nightly only features in the formatting step of the pre-commit script

--- infra/scripts/pre-commit.sh
+++ infra/scripts/pre-commit.sh
@@ -8,7 +8,7 @@ echo 'Formatting...'
  for rust_file in $(git diff --name-only --cached | grep ".*\.rs$"); do
 
     if test -e "$rust_file"; then
-        cargo fmt -- "$rust_file"
+        rustup run nightly cargo fmt -- "$rust_file"
         git add "$rust_file"
     fi
  done
