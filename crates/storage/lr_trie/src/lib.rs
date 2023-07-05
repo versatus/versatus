d16d4b41889131017416452c9a7db9556bc26370
Chris-san <57543709+eureka-cpu@users.noreply.github.com>
2023-07-04 20:27:50 -0500

    fix: update `keccak_hash::H256` with `patriecia::H256` (#401)
    Makes use of `patriecia::serde_hash::H256` which is an adaptation of `keccak_hash::H256` that implements `serde`'s derived macros for `Serialize` & `Deserialize`. Handles breaking changes introduced by https://github.com/vrrb-io/patriecia/pull/15

--- crates/storage/lr_trie/src/lib.rs
+++ crates/storage/lr_trie/src/lib.rs
@@ -1,6 +1,6 @@
 /// This crate contains a left-right wrapped, evmap-backed, Merkle-Patricia Trie
 /// heavily inspired by https://github.com/carver/eth-trie.rs which is a fork of https://github.com/citahub/cita-trie
-pub use keccak_hash::H256;
+pub use patriecia::H256;
 
 mod inner;
 mod inner_wrapper;
