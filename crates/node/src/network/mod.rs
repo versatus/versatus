83028145be4bb0547bf73ee6cf42134d33e0c9a6
Daniel M <dmartinez@vrrb.io>
2023-08-01 17:45:38 +0200

    refactor network component

--- crates/node/src/network/mod.rs
+++ crates/node/src/network/mod.rs
@@ -1,7 +1,9 @@
-mod module;
+mod component;
+mod handler;
 mod network_event;
 mod network_event_handler;
 
-pub use module::*;
+pub use component::*;
+pub use handler::*;
 pub use network_event::*;
 pub use network_event_handler::*;
