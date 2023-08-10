1725c10faefb3c9300cf7e64e8398e52183eccd5
Daniel <dm@versatus.io>
2023-08-10 13:17:53 +0200

    Define StateReader trait (#429)

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -12,6 +12,7 @@ pub(crate) mod mining_module;
 pub(crate) mod network;
 pub(crate) mod runtime;
 pub(crate) mod state_manager;
+pub(crate) mod state_reader;
 pub(crate) mod ui;
 
 pub mod test_utils;
