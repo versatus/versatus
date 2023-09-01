a0089153dd831b378609b3a7711b0a13d85f3c15
Matthew Geddes <mgeddes@vrrb.io>
2023-09-01 11:51:35 -0700

    Initial framework for compute agent and shared service config (#446)
    Co-authored-by: Matthew Geddes <mg@versatus.io>

---
+++ crates/compute_agent/src/commands/status/mod.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use clap::Parser;
+use service_config::ServiceConfig;
+
+/// Command line options structure for status subcommand
+#[derive(Parser, Debug)]
+pub struct StatusOpts {
+}
+
+/// Make a status RPC query against a running agent.
+pub async fn run(opts: &StatusOpts, config: &ServiceConfig) -> Result<()> {
+    // XXX: This where we would make the status RPC call to the named service (global option) from
+    // the service config file (global option) and show the result.
+    Ok(())
+}
