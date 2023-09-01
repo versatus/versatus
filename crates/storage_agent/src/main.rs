5fe930cda986065b7cfa82929fe7a2059e1943f7
Matthew Geddes <mg@versatus.io>
2023-09-01 11:55:56 -0700

    Initial pass and CLI arg parsing for storage agent

---
+++ crates/storage_agent/src/main.rs
@@ -0,0 +1,40 @@
+mod cli;
+mod commands;
+
+use anyhow::Result;
+use clap::Parser;
+use tokio;
+use service_config::Config;
+use telemetry::info;
+
+static THIS_SERVICE_TYPE: &str = "storage";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = cli::StorageCli::parse();
+
+    let service: String;
+    match cli.service_type {
+        Some(svc) => service = svc.clone(),
+        None => service = THIS_SERVICE_TYPE.to_string(),
+    }
+
+    // Parse common services configuration
+    let config = Config::from_file(&cli.config)?
+        .find_service(&cli.service, &service)?;
+
+    info!("Matched service {}:{} to config: {:?}", cli.service, service, config);
+
+    // Process subcommand
+    match &cli.cmd {
+        Some(cli::StorageCommands::Daemon(opts)) => {
+            commands::daemon::run(opts, &config).await?;
+        },
+        Some(cli::StorageCommands::Status(opts)) => {
+            commands::status::run(opts, &config).await?;
+        },
+        None => {},
+    }
+
+    Ok(())
+}
