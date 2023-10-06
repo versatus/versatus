5140cfa08fd0afd966e6283571120da29988d8e9
Mitch Martin <mm@versatus.io>
2023-10-06 15:06:46 -0500

    faucet toml setup

--- crates/faucet/src/faucet.rs
+++ crates/faucet/src/faucet.rs
@@ -3,6 +3,7 @@ use serde::Deserialize;
 use serde_json::json;
 use std::{convert::Infallible, net::SocketAddr};
 use tokio::spawn;
+use wallet::v2::{Wallet, WalletConfig};
 
 #[derive(Deserialize)]
 struct FaucetRequest {
@@ -12,7 +13,13 @@ struct FaucetRequest {
 pub struct FaucetConfig {
     pub rpc_server_address: String,
     pub server_port: u16,
+    pub wallet_mnemonic: String,
     // Add other configuration parameters if needed.
+}
+
+struct Faucet {
+    config: FaucetConfig,
+    wallet: Wallet,
 }
 
 async fn faucet(req: axum::extract::Json<FaucetRequest>) -> Result<axum::response::Json<serde_json::Value>, StatusCode> {
@@ -29,24 +36,25 @@ async fn faucet(req: axum::extract::Json<FaucetRequest>) -> Result<axum::respons
     Ok(axum::response::Json(json!({"message": "RPC Server Set"})))
 }
 
-pub async fn start_server(config: FaucetConfig) -> Result<(), Box<dyn std::error::Error>> {
+impl Faucet {
+    pub async fn start(config: FaucetConfig) -> Result<(), Box<dyn std::error::Error>> {
+        let mut wallet_config = WalletConfig::default();
+        wallet_config.rpc_server_address = config.rpc_server_address;
 
-    let mut wallet_config = WalletConfig::default();
-    wallet_config.rpc_server_address = config.rpc_server_address;
+        let mut wallet = Wallet::new(wallet_config).await.unwrap();
 
-    let mut wallet = Wallet::new(wallet_config).await.unwrap();
+        let app = Router::new()
+            .route("/faucet", post(faucet))
+            .handle_error(|error: axum::Error| {
+                let (status, _response_body) = (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {:?}", error),
+                );
+                Ok::<_, Infallible>((status, _response_body))
+            });
 
-    let app = Router::new()
-        .route("/faucet", post(faucet))
-        .handle_error(|error: axum::Error| {
-            let (status, _response_body) = (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Unhandled internal error: {:?}", error),
-            );
-            Ok::<_, Infallible>((status, _response_body))
-        });
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], config.server_port));
-    println!("Server started at http://{}", addr);
-    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+        let addr = SocketAddr::from(([127, 0, 0, 1], config.server_port));
+        println!("Server started at http://{}", addr);
+        axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+    }
 }
