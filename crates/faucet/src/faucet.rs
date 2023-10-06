5c7137783b34e24f52b61b2f9f0e445adbd1da85
Mitch Martin <mm@versatus.io>
2023-10-06 14:33:53 -0500

    faucet

---
+++ crates/faucet/src/faucet.rs
@@ -0,0 +1,52 @@
+use axum::{handler::post, http::StatusCode, Router};
+use serde::Deserialize;
+use serde_json::json;
+use std::{convert::Infallible, net::SocketAddr};
+use tokio::spawn;
+
+#[derive(Deserialize)]
+struct FaucetRequest {
+    account: String,
+}
+
+pub struct FaucetConfig {
+    pub rpc_server_address: String,
+    pub server_port: u16,
+    // Add other configuration parameters if needed.
+}
+
+async fn faucet(req: axum::extract::Json<FaucetRequest>) -> Result<axum::response::Json<serde_json::Value>, StatusCode> {
+    println!("Setting RPC server address: {}", req.rpc_server);
+
+    // Here, you should call your crate's function to handle transactions.
+    // spawn(your_crate::start_transaction_handler(req.rpc_server));
+
+    // Simulating the process with a dummy async block.
+    spawn(async {
+        println!("Dummy transaction handler started with RPC: {}", req.rpc_server);
+    });
+
+    Ok(axum::response::Json(json!({"message": "RPC Server Set"})))
+}
+
+pub async fn start_server(config: FaucetConfig) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut wallet_config = WalletConfig::default();
+    wallet_config.rpc_server_address = config.rpc_server_address;
+
+    let mut wallet = Wallet::new(wallet_config).await.unwrap();
+
+    let app = Router::new()
+        .route("/faucet", post(faucet))
+        .handle_error(|error: axum::Error| {
+            let (status, _response_body) = (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Unhandled internal error: {:?}", error),
+            );
+            Ok::<_, Infallible>((status, _response_body))
+        });
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.server_port));
+    println!("Server started at http://{}", addr);
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+}
