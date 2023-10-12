9cb84d111c22b4dd56be82035cf390fe5f5dcb62
Matt Geddes <mg@versatus.io>
2023-10-11 22:38:56 -0700

    #566: Cgroup metrics from generic platform crate

---
+++ crates/platform/src/lib.rs
@@ -0,0 +1,4 @@
+/// This crate contains a bunch of platform-related functionality. Specifically
+/// the Linux operating system for the moment, but could be extended to support
+/// other operating systems at least in part in the future.
+pub mod platform_stats;
