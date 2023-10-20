c6a63aff890ca34b20d06ff49942f34ddb2aee9a
Andrew O'Brien <ohbandrew@gmail.com>
2023-10-20 15:27:00 -0500

    modified subscriber.rs to appease sir clippy

--- crates/telemetry/src/lib.rs
+++ crates/telemetry/src/lib.rs
@@ -1,8 +1,8 @@
 /// Exposes some useful utilities around tracing.
 /// Re-exports everything on tracing to avoid having to import tracing
 /// everywhere along with this crate
+pub mod custom_subscriber;
 mod metrics;
-mod subscriber;
 pub use metrics::*;
 pub use subscriber::*;
 pub use tracing::{self, *};
