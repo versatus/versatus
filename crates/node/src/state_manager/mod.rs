70995c2825f9645b4819dc759b03178215d634fb
eureka-cpu <github.eureka@gmail.com>
2023-11-13 21:01:24 -0600

    remove ignore attribute on test that is passing

--- crates/node/src/state_manager/mod.rs
+++ crates/node/src/state_manager/mod.rs
@@ -70,7 +70,7 @@ mod tests {
     pub type StateDag = Arc<RwLock<BullDag<Block, BlockHash>>>;
 
     #[tokio::test]
-    #[ignore]
+    #[serial]
     async fn vrrbdb_should_update_with_new_block() {
         let db_config = VrrbDbConfig::default().with_path(std::env::temp_dir().join("db"));
         let db = VrrbDb::new(db_config);
