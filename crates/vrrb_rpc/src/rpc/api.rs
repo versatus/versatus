5812312a9a52ee5da7fd4cfa60742e8d104e1421
Chris-san <57543709+eureka-cpu@users.noreply.github.com>
2023-11-17 17:49:33 -0600

    vrrb_rpc general cleanup (#681)

--- crates/vrrb_rpc/src/rpc/api.rs
+++ crates/vrrb_rpc/src/rpc/api.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use block::block::Block;
 use block::ClaimHash;
-use jsonrpsee::{core::Error, proc_macros::rpc};
+use jsonrpsee::{core::Error as RpseeError, proc_macros::rpc};
 use primitives::{Address, NodeType, Round};
 use secp256k1::PublicKey;
 use serde::{Deserialize, Serialize};
@@ -22,9 +22,7 @@ pub type FullStateSnapshot = HashMap<Address, Account>;
 pub type FullMempoolSnapshot = Vec<RpcTransactionRecord>;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct TransactionRecord {
-    //
-}
+pub struct TransactionRecord;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct FullMempoolSnapshotResponse {
@@ -67,79 +65,79 @@ impl From<TransactionKind> for RpcTransactionRecord {
 pub trait RpcApi {
     /// Returns a full list of all accounts within state
     #[method(name = "getFullState")]
-    async fn get_full_state(&self) -> Result<FullStateSnapshot, Error>;
+    async fn get_full_state(&self) -> Result<FullStateSnapshot, RpseeError>;
 
     /// Returns a full list of transactions pending to be confirmed
     #[method(name = "getFullMempool")]
-    async fn get_full_mempool(&self) -> Result<FullMempoolSnapshot, Error>;
+    async fn get_full_mempool(&self) -> Result<FullMempoolSnapshot, RpseeError>;
 
     /// Returns the node type this client is connected to
     #[method(name = "getNodeType")]
-    async fn get_node_type(&self) -> Result<NodeType, Error>;
+    async fn get_node_type(&self) -> Result<NodeType, RpseeError>;
 
     /// Create a new transaction
     #[method(name = "createTxn")]
-    async fn create_txn(&self, txn: TransactionKind) -> Result<RpcTransactionRecord, Error>;
+    async fn create_txn(&self, txn: TransactionKind) -> Result<RpcTransactionRecord, RpseeError>;
 
     /// Get a transaction from state
     #[method(name = "getTransaction")]
     async fn get_transaction(
         &self,
         transaction_digest: RpcTransactionDigest,
-    ) -> Result<RpcTransactionRecord, Error>;
+    ) -> Result<RpcTransactionRecord, RpseeError>;
 
     /// List a group of transactions
     #[method(name = "listTransactions")]
     async fn list_transactions(
         &self,
         digests: Vec<RpcTransactionDigest>,
-    ) -> Result<HashMap<RpcTransactionDigest, RpcTransactionRecord>, Error>;
+    ) -> Result<HashMap<RpcTransactionDigest, RpcTransactionRecord>, RpseeError>;
 
     #[method(name = "createAccount")]
-    async fn create_account(&self, address: Address, account: Account) -> Result<(), Error>;
+    async fn create_account(&self, address: Address, account: Account) -> Result<(), RpseeError>;
 
     #[method(name = "updateAccount")]
-    async fn update_account(&self, account: Account) -> Result<(), Error>;
+    async fn update_account(&self, account: Account) -> Result<(), RpseeError>;
 
     #[method(name = "getAccount")]
-    async fn get_account(&self, address: Address) -> Result<Account, Error>;
+    async fn get_account(&self, address: Address) -> Result<Account, RpseeError>;
 
     #[method(name = "faucetDrip")]
-    async fn faucet_drip(&self, address: Address) -> Result<(), Error>;
+    async fn faucet_drip(&self, address: Address) -> Result<(), RpseeError>;
 
     #[method(name = "signTransaction")]
-    async fn sign_transaction(&self, sign_opts: SignOpts) -> Result<String, Error>;
+    async fn sign_transaction(&self, sign_opts: SignOpts) -> Result<String, RpseeError>;
 
     #[method(name = "getRound")]
-    async fn get_round(&self) -> Result<Round, Error>;
+    async fn get_round(&self) -> Result<Round, RpseeError>;
 
     #[method(name = "getBlocks")]
-    async fn get_blocks(&self) -> Result<Vec<Block>, Error>;
+    async fn get_blocks(&self) -> Result<Vec<Block>, RpseeError>;
 
     #[method(name = "getProgram")]
-    async fn get_program(&self) -> Result<(), Error>;
+    async fn get_program(&self) -> Result<(), RpseeError>;
 
     #[method(name = "callProgram")]
-    async fn call_program(&self) -> Result<(), Error>;
+    async fn call_program(&self) -> Result<(), RpseeError>;
 
     #[method(name = "getTransactionCount")]
-    async fn get_transaction_count(&self, account: Address) -> Result<usize, Error>;
+    async fn get_transaction_count(&self, account: Address) -> Result<usize, RpseeError>;
 
     #[method(name = "getNodeHealth")]
-    async fn get_node_health(&self) -> Result<NodeHealthReport, Error>;
+    async fn get_node_health(&self) -> Result<NodeHealthReport, RpseeError>;
 
     #[method(name = "getClaimsByAccountId")]
-    async fn get_claims_by_account_id(&self, address: Address) -> Result<Claims, Error>;
+    async fn get_claims_by_account_id(&self, address: Address) -> Result<Claims, RpseeError>;
 
     #[method(name = "getClaimHashes")]
-    async fn get_claim_hashes(&self) -> Result<Vec<ClaimHash>, Error>;
+    async fn get_claim_hashes(&self) -> Result<Vec<ClaimHash>, RpseeError>;
 
     #[method(name = "getClaims")]
-    async fn get_claims(&self, claim_hashes: Vec<ClaimHash>) -> Result<Claims, Error>;
+    async fn get_claims(&self, claim_hashes: Vec<ClaimHash>) -> Result<Claims, RpseeError>;
 
     #[method(name = "getMembershipConfig")]
-    async fn get_membership_config(&self) -> Result<QuorumMembershipConfig, Error>;
+    async fn get_membership_config(&self) -> Result<QuorumMembershipConfig, RpseeError>;
 
     #[method(name = "getLastBlock")]
-    async fn get_last_block(&self) -> Result<Block, Error>;
+    async fn get_last_block(&self) -> Result<Block, RpseeError>;
 }
