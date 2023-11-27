50e49a0f4d148243edefc8b783031edb3c38281a
Andrew O'Brien <ohbandrew@gmail.com>
2023-11-27 13:31:25 -0600

    changes to out_of_memory.wasm

--- crates/wasm_runtime/src/rust2wasm/mod.rs
+++ crates/wasm_runtime/src/rust2wasm/mod.rs
@@ -1,4 +1,5 @@
 //! Rust programs that should compile to the wasm32-wasi target
 //! and be stored in the test_data directory.
-pub mod infinite_recursion;
 pub mod file_not_found;
+pub mod infinite_recursion;
+pub mod trigger_oom;
