094bc126642a79c22cf93bfa5333f341ed7f0d1e
Andrew O'Brien <ohbandrew@gmail.com>
2023-11-30 21:51:54 -0600

    set up builder pattern for HttpApiRouterConfig

--- crates/vrrb_rpc/src/http/config.rs
+++ crates/vrrb_rpc/src/http/config.rs
@@ -22,3 +22,44 @@ pub struct HttpApiRouterConfig {
     pub api_version: String,
     pub server_timeout: Option<Duration>,
 }
+
+impl From<HttpApiRouterConfigBuilder> for HttpApiRouterConfig {
+    fn from(value: HttpApiRouterConfigBuilder) -> HttpApiRouterConfig {
+        HttpApiRouterConfig {
+            address: value.address.expect("expected socket address"),
+            api_title: value.api_title.expect("expected router api title"),
+            api_version: value.api_version.expect("expected router api version"),
+            server_timeout: value.server_timeout,
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct HttpApiRouterConfigBuilder {
+    pub address: Option<SocketAddr>,
+    pub api_title: Option<String>,
+    pub api_version: Option<String>,
+    pub server_timeout: Option<Duration>,
+}
+
+impl HttpApiRouterConfigBuilder {
+    pub fn address(mut self, address: SocketAddr) -> Self {
+        self.address = Some(address.into());
+        self
+    }
+    pub fn api_title(mut self, api_title: &str) -> Self {
+        self.api_title = Some(api_title.into());
+        self
+    }
+    pub fn api_version(mut self, api_version: &str) -> Self {
+        self.api_version = Some(api_version.into());
+        self
+    }
+    pub fn server_timeout(mut self, server_timeout: Option<Duration>) -> Self {
+        self.server_timeout = server_timeout;
+        self
+    }
+    pub fn build(self) -> HttpApiRouterConfig {
+        self.into()
+    }
+}
