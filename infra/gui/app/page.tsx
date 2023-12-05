7fe0fa1b17f6dede8622f9b421619a3cef9058a0
Andrew Hathaway <hathbanger.ah@gmail.com>
2023-12-05 09:40:10 -0600

    update node gui w more data around quorum and quorum kind

--- infra/gui/app/page.tsx
+++ infra/gui/app/page.tsx
@@ -1,33 +1,45 @@
 'use client'
-import { NodeProvider } from '@/contexts/NodeProvider'
+import {NodeProvider, useNodeContext} from '@/contexts/NodeProvider'
 import { NodeStatusModule } from '@/components/modules/NodeStatusModule'
 import { Wind2 } from 'iconsax-react'
+import {ReactNode} from "react";
 
 export default function Home() {
-  const PageHeader = () => {
-    return (
-      <div
-        className={
-          'gap-2 text-white flex flex-row rounded p-0 m-0 items-center'
-        }
-      >
-        <Wind2 />
-        <div className={'text-md font-light text-white'}>
-          Farmer Node Overview
-        </div>
-      </div>
-    )
-  }
+
+
   return (
     <div>
       <NodeProvider>
-        <div className={'flex flex-col gap-3  mx-4'}>
-          <PageHeader />
-          <div className={'grid grid-cols-1 lg:grid-cols-3 gap-8'}>
-            <NodeStatusModule />
-          </div>
-        </div>
+        <AppContainer />
       </NodeProvider>
     </div>
+  )
+}
+
+const AppContainer = () => {
+
+  return (
+      <div className={'flex flex-col gap-3  mx-4'}>
+        <PageHeader />
+        <div className={'grid grid-cols-1 lg:grid-cols-2 gap-8'}>
+          <NodeStatusModule />
+        </div>
+      </div>
+  )
+}
+
+const PageHeader = () => {
+  const {nodeType} = useNodeContext()
+  return (
+      <div
+          className={
+            'gap-2 text-white flex flex-row rounded p-0 m-0 items-center'
+          }
+      >
+        <Wind2 />
+        <div className={'text-md font-light text-white'}>
+          {nodeType?.result.slice(0,1).toUpperCase() + nodeType?.result.slice(1, nodeType?.result.length).toLowerCase()} Node Overview
+        </div>
+      </div>
   )
 }
--- infra/gui/contexts/NodeProvider.tsx
+++ infra/gui/contexts/NodeProvider.tsx
@@ -4,17 +4,39 @@ import useSWR from 'swr'
 
 interface NodeContextProps {
   fullState: any
-  fullStateLoading: boolean
+  fullStateLoading: boolean | undefined
   fullStateErr: any
   memPool: any[]
-  memPoolLoading: boolean
+  memPoolLoading: boolean | undefined
   memPoolErr: any
-  nodeType: string | null | undefined
-  nodeTypeLoading: boolean
+  nodeType: any
+  nodeTypeLoading: boolean | undefined
   nodeTypeErr: any
+  membershipConfig: any
+  membershipConfigLoading: boolean | undefined
+  membershipConfigErr: any
+  lastBlock: any
+  lastBlockLoading: boolean | undefined
+  lastBlockErr: any
+  transactionCount: number;
+  transactionCountLoading: boolean | undefined;
+  transactionCountErr: any;
+  nodeHealth: any;
+  nodeHealthLoading: boolean | undefined;
+  nodeHealthErr: any;
+  claimsByAccountId: any;
+  claimsByAccountIdLoading: boolean | undefined;
+  claimsByAccountIdErr: any;
+  claimHashes: any[];
+  claimHashesLoading: boolean | undefined;
+  claimHashesErr: any;
+  claims: any;
+  claimsLoading: boolean | undefined;
+  claimsErr: any;
   account: any
   address: string
   setAddress: (address: string) => void
+
 }
 
 const NodeContext = createContext<NodeContextProps>({
@@ -30,6 +52,27 @@ const NodeContext = createContext<NodeContextProps>({
   nodeType: undefined,
   nodeTypeLoading: false,
   nodeTypeErr: undefined,
+  membershipConfig: undefined,
+  membershipConfigLoading: false,
+  membershipConfigErr: undefined,
+  lastBlock: undefined,
+  lastBlockLoading: false,
+  lastBlockErr: undefined,
+  transactionCount: 0,
+  transactionCountLoading: false,
+  transactionCountErr: undefined,
+  nodeHealth: undefined,
+  nodeHealthLoading: false,
+  nodeHealthErr: undefined,
+  claimsByAccountId: undefined,
+  claimsByAccountIdLoading: false,
+  claimsByAccountIdErr: undefined,
+  claimHashes: [],
+  claimHashesLoading: false,
+  claimHashesErr: undefined,
+  claims: undefined,
+  claimsLoading: false,
+  claimsErr: undefined,
 } as NodeContextProps)
 
 const useNodeContext = () => useContext(NodeContext)
@@ -56,13 +99,59 @@ const NodeProvider = ({ children }: {children: ReactNode}) => {
     error: nodeTypeErr,
   } = useSWR({ url: 'getNodeType' }, rpcFetcher)
 
+  const {
+    data: membershipConfig,
+    isLoading: membershipConfigLoading,
+    error: membershipConfigErr,
+  } = useSWR({ url: 'getMembershipConfig' }, rpcFetcher)
+
+  const {
+    data: lastBlock,
+    isLoading: lastBlockLoading,
+    error: lastBlockErr,
+  } = useSWR({ url: 'getLastBlock' }, rpcFetcher)
+
+  const {
+    data: nodeHealth,
+    isLoading: nodeHealthLoading,
+    error: nodeHealthErr,
+  } = useSWR({ url: 'getNodeHealth' }, rpcFetcher);
+
+  const {
+    data: claimHashes,
+    isLoading: claimHashesLoading,
+    error: claimHashesErr,
+  } = useSWR({ url: 'getClaimHashes' }, rpcFetcher);
+
+
+
+  //TODO: swrs need args under here
+  const {
+    data: transactionCount,
+    isLoading: transactionCountLoading,
+    error: transactionCountErr,
+  } = useSWR({ url: 'getTransactionCount' }, rpcFetcher);
+
+  const {
+    data: claims,
+    isLoading: claimsLoading,
+    error: claimsErr,
+  } = useSWR({ url: 'getClaims' }, rpcFetcher);
+
+  const {
+    data: claimsByAccountId,
+    isLoading: claimsByAccountIdLoading,
+    error: claimsByAccountIdErr,
+  } = useSWR({ url: 'getClaimsByAccountId' }, rpcFetcher);
+
+
+
+
   useEffect(() => {
     if (fullState && Object.keys(fullState).length > 0) {
       setAddress(Object.keys(fullState)[0])
     }
   }, [fullState])
-
-  console.log('fullState', fullState)
 
   useEffect(() => {
     getAccount(
@@ -85,6 +174,27 @@ const NodeProvider = ({ children }: {children: ReactNode}) => {
         nodeType,
         nodeTypeLoading,
         nodeTypeErr,
+        membershipConfig,
+        membershipConfigLoading,
+        membershipConfigErr,
+        lastBlock,
+        lastBlockLoading,
+        lastBlockErr,
+        transactionCount,
+        transactionCountLoading,
+        transactionCountErr,
+        nodeHealth,
+        nodeHealthLoading,
+        nodeHealthErr,
+        claimsByAccountId,
+        claimsByAccountIdLoading,
+        claimsByAccountIdErr,
+        claimHashes,
+        claimHashesLoading,
+        claimHashesErr,
+        claims,
+        claimsLoading,
+        claimsErr,
         account,
         address,
         setAddress,
