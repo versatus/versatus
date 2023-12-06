38a211b0c343a4d30e618ac2dbd2dac0531f49c4
Andrew O'Brien <ohbandrew@gmail.com>
2023-12-06 13:19:00 -0600

    update file_not_found .wasm and rust2wasm code

--- crates/wasm_runtime/src/rust2wasm/file_not_found.rs
+++ crates/wasm_runtime/src/rust2wasm/file_not_found.rs
@@ -1,13 +1,7 @@
 use std::fs::File;
-use std::io::Read;
 #[allow(dead_code)]
 fn main() {
     let file_path = "non_existent_file.txt";
 
     let mut file = File::open(file_path).unwrap();
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-
-    println!("File contents: {}", contents);
 }
