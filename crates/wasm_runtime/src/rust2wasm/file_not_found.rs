814752a33f089af90cbe44b4ec4e0a19539f174d
Andrew O'Brien <ohbandrew@gmail.com>
2023-12-06 13:28:05 -0600

    update file_not_found again

--- crates/wasm_runtime/src/rust2wasm/file_not_found.rs
+++ crates/wasm_runtime/src/rust2wasm/file_not_found.rs
@@ -1,7 +1,4 @@
-use std::fs::File;
 #[allow(dead_code)]
 fn main() {
-    let file_path = "non_existent_file.txt";
-
-    let mut file = File::open(file_path).unwrap();
+    std::fs::File::open("non_existent_file.txt").unwrap();
 }
