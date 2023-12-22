43f5b60cd087601b7d0465e2129522ed1aa853c5
Chris-san <57543709+eureka-cpu@users.noreply.github.com>
2023-12-22 12:03:22 -0600

    Add RPC client to compute CLIs (#733)

--- crates/internal_rpc/src/api.rs
+++ crates/internal_rpc/src/api.rs
@@ -1,6 +1,5 @@
 use jsonrpsee::proc_macros::rpc;
 use platform::services::ServiceStatusResponse;
-use service_config::ServiceConfig;
 
 pub(crate) type RpcResult<T> = Result<T, jsonrpsee::core::Error>;
 
@@ -10,17 +9,7 @@ pub(crate) type RpcResult<T> = Result<T, jsonrpsee::core::Error>;
 /// This is meant to be extensible to meet the needs of the `InternalRpcServer`.
 #[rpc(server, client, namespace = "common")]
 pub trait InternalRpcApi {
-    /// Get the status of a job
-    #[method(name = "status")]
-    async fn status(&self) -> RpcResult<()> {
-        Ok(())
-    }
-
     /// Get info about the current service
-    #[method(name = "serviceStatusResponse")]
-    async fn service_status_response(&self) -> RpcResult<ServiceStatusResponse>;
-
-    /// Returns a copy of the [`ServiceConfig`] the server was constructed with
-    #[method(name = "serviceConfig")]
-    async fn service_config(&self) -> RpcResult<ServiceConfig>;
+    #[method(name = "status")]
+    async fn status(&self) -> RpcResult<ServiceStatusResponse>;
 }
