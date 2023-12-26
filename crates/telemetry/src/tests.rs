d44193d642eb0dfc7c04cadf7d4bc3bc699ec4fb
Matthew Geddes <musicalcarrion@gmail.com>
2023-12-25 18:04:39 -0800

    Added some code for runtime stats

---
+++ crates/telemetry/src/tests.rs
@@ -0,0 +1,17 @@
+// Tests for request stats
+use crate::request_stats::RequestStats;
+use std::thread::sleep;
+use std::time::Duration;
+
+#[test]
+fn req_stats_ok() {
+    let _ = env_logger::builder().is_test(true).try_init();
+    let mut stats = RequestStats::new("Test".to_string(), "req_stats_ok".to_string())
+        .expect("Failed to create new stats object");
+    stats.start("1sec".to_string()).expect("Failed to start");
+    sleep(Duration::from_secs(1));
+    stats.stop("1sec".to_string()).expect("Failed to stop");
+    stats.start("3sec".to_string()).expect("Failed to start");
+    sleep(Duration::from_secs(3));
+    stats.stop("3sec".to_string()).expect("Failed to stop");
+}
