3f8a45efe4b51ffbcedd3a7760a68fac8c000062
Andrew O'Brien <ohbandrew@gmail.com>
2024-01-05 21:24:52 -0600

    fmt

--- crates/wasm_cli/src/commands/mod.rs
+++ crates/wasm_cli/src/commands/mod.rs
@@ -1,7 +1,7 @@
-pub mod testbalance;
 pub mod describe;
 pub mod execute;
-pub mod testinitdb;
-pub mod testcontract;
 pub mod publish;
+pub mod testbalance;
+pub mod testcontract;
+pub mod testinitdb;
 pub mod validate;
