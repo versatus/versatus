e1901d22b3e19f1f1d25f0702750bb1c380627c2
eureka-cpu <github.eureka@gmail.com>
2024-01-10 15:22:46 -0600

    add job type enum stub

--- crates/compute_agent/src/commands/compute_job/job_status.rs
+++ crates/compute_agent/src/commands/compute_job/job_status.rs
@@ -1,19 +1,18 @@
 use crate::commands::compute_job::ComputeJobOpts;
 use anyhow::Result;
 use service_config::ServiceConfig;
+use std::time::Duration;
 
 pub enum ComputeJobStatus {
     /// Job is in progress, represents the uptime of the job.
-    Running(std::time::Duration),
+    Running(Duration),
     /// Job is completed, represents the time the job took to execute.
-    Complete(std::time::Duration),
+    Complete(Duration),
 }
 
 // Should return the status of the job
 pub async fn run(_opts: &ComputeJobOpts, _config: &ServiceConfig) -> Result<ComputeJobStatus> {
     // Connect to the server and request the status of a job via stringified CID.
     // The server should return the job's status.
-    Ok(ComputeJobStatus::Complete(std::time::Duration::from_secs(
-        0,
-    )))
+    Ok(ComputeJobStatus::Complete(Duration::from_secs(0)))
 }
--- crates/compute_agent/src/commands/compute_job/mod.rs
+++ crates/compute_agent/src/commands/compute_job/mod.rs
@@ -7,4 +7,16 @@ pub mod queue_job;
 pub struct ComputeJobOpts {
     #[clap(long)]
     cid: String,
+    #[clap(long, short = 'j')]
+    job_type: Option<ComputeJobType>,
+}
+
+pub enum ComputeJobType {
+    Unknown,
+}
+impl std::str::FromStr for ComputeJobType {
+    type Err = anyhow::Error;
+    fn from_str(_s: &str) -> Result<Self, Self::Err> {
+        Ok(ComputeJobType::Unknown)
+    }
 }
