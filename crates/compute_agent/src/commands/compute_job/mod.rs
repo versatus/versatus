be664aef8a0d9bbef96c6d458628d7e5057a44ec
eureka-cpu <github.eureka@gmail.com>
2024-01-10 19:10:48 -0600

    add job input path cli opt

--- crates/compute_agent/src/commands/compute_job/mod.rs
+++ crates/compute_agent/src/commands/compute_job/mod.rs
@@ -7,11 +7,16 @@ pub mod queue_job;
 /// Options to pass to subcommands pertaining to compute jobs.
 #[derive(Parser)]
 pub struct ComputeJobOpts {
-    /// A string representation of a compute job's CID
+    /// A string representation of a compute job's CID.
     #[clap(long)]
     cid: String,
 
-    /// The type of compute job to add to the job queue
+    /// The type of compute job to add to the job queue.
     #[clap(long, short = 'j')]
     job_type: Option<ComputeJobExecutionType>,
+
+    /// The path to a JSON file that represents the inputs
+    /// for a compute job.
+    #[clap(long, short = 'p')]
+    input_path: Option<String>,
 }
